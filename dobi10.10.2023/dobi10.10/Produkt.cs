52a23fb12082194226af0729eb79355c99116841
ks4wI <148214067+ks4wI@users.noreply.github.com>
2023-10-24 00:26:03 +0200

    Add files via upload

---
+++ dobi10.10.2023/dobi10.10/Produkt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dobi10._10
+{
+    internal class Produkt
+    {
+        public string nazwaProducenta;
+        public string nazwaProduktu;
+        public string kategoria;
+        public string kodProduktu;
+        public int cena;
+        public string opis;
+
+        public Produkt(string nazwaProducenta, string nazwaProduktu, string kategoria, string kodProduktu, int cena, string opis)
+        {
+            this.nazwaProducenta = nazwaProducenta;
+            this.nazwaProduktu = nazwaProduktu;
+            this.kategoria = kategoria;
+            this.kodProduktu = kodProduktu;
+            this.cena = cena;
+            this.opis = opis;
+        }
+    }
+}
